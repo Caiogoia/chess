2c06247f96313315459447ee74428c03a56dfcb2
Caiogoia <caiogoia390@gmail.com>
2021-05-24 11:39:00 -0300

    [dev] something

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,9 +1,19 @@
-const Table = () => {
+const Table = ({getCoordinates}) => {
+    const board = createBoard
+
+    const createBoard = () => {
+        let arr = []
+        for(let i = 0; i < 8; i++){
+            for(let j = 0; j < 8; j++){
+                arr[i].push
+            }
+        }
+        return arr
+    }
+    
     return(
         <h1>Table</h1>
-        {Array.from( {length: 64} ).map(_, index) => {
-
-        }}
+        
     )
 }
 
