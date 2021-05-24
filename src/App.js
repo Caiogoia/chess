304f62f6a6754595fd25a14137edf3389dfffc3f
Andre Rodrigues Martini <andre.martini@venturus.org.br>
2021-05-24 11:12:42 -0300

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Grid } from '@material-ui/core';
+import History from './components/history'
+import Table from './components/table'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+        <Grid item xs={8}>
+          <Table/>
+        </Grid>
+        <Grid item xs={8}>
+          <History/>
+      </Grid>
+    </Grid>
   );
 }
 
---
+++ src/components/history/index.js
@@ -0,0 +1,11 @@
+import { Box } from '@material-ui/core'
+
+const History = () => {
+    return(
+       <Box>
+           <h1>History</h1>
+       </Box>
+    )
+}
+
+export default History
