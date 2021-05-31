bdfca6a2ec97739aff593dc9cd8c24a9d5f29b13
Caiogoia <caiogoia390@gmail.com>
2021-05-31 10:53:49 -0300

    commit do Caio

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import { Grid } from "@material-ui/core";
 import History from "./components/history";
 import Table from "./components/table";
 
-import useStyles from './styleApp'
+import useStyles from "./styleApp";
 
 function App() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <Grid container className={classes.container} spacing={3}>
-      <Grid item xs={8} style={{background:"purple"}}>
-        <h1>table</h1>
-        {/* <Table/> */}
+      <Grid item xs={8}>
+        {/* <h1>table</h1> */}
+        <Table />
       </Grid>
       <Grid item xs={4}>
         <History />
--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,20 +1,25 @@
-// const Table = ({getCoordinates}) => {
-//     const board = createBoard
+import { Rect, Stage, Layer } from "react-konva";
 
-//     const createBoard = () => {
-//         let arr = []
-//         for(let i = 0; i < 8; i++){
-//             for(let j = 0; j < 8; j++){
-//                 arr[i].push
-//             }
-//         }
-//         return arr
-//     }
+const Table = ({ getCoordinates }) => {
+  const arr = createBoard();
 
-//     return(
-//         <h1>Table</h1>
+  const createBoard = () => {
+    let arrFunc = [];
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        arrFunc[i].push({ width: 50, height: 50 });
+      }
+    }
+    return arrFunc;
+  };
 
-//     )
-// }
+  return (
+    <Stage width={500} height={500}>
+      <Layer>
+        <Rect width={50} height={50} fill="black" />
+      </Layer>
+    </Stage>
+  );
+};
 
-// export default Table
+export default Table;
