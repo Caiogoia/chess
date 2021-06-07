620452d149b1e1a7f01680de52a4d05907176ca8
Francielle Dellamora Menegotto <francidellamora@gmail.com>
2021-06-07 11:32:25 -0300

    jogadas

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import { Grid } from "@material-ui/core";
 import History from "./components/history";
 import Table from "./components/table";
+import { useState } from "react";
 
 import useStyles from "./styleApp";
 
 function App() {
-  const jogadas = ["jogada1", "jogada1", "jogada1"];
-
+  const [coordinates, setCoordinates] = useState();
   const handleClick = (letter, number) => {
     console.log([letter, number]);
   };
-  // const classes = useStyles()
-  // const [position, setPosition] = useState({ x: 1, y: 1 })
-  // const [history] = useState([])
 
-  // const onPositionChanged = pos => {
-  //   setPosition(pos)
-  //   history.push(pos)
-  // }
-
-  // const undo = () => {
-  //   if (history.length > 0) {
-  //     let pos = history.pop()
-  //     setPosition(pos)
-  //   }
-  // }
   const classes = useStyles();
   return (
     <Grid container className={classes.container} spacing={3}>
-      <Grid item xs={8}>
+      <Grid item xs={4}>
         {/* <h1>table</h1> */}
         <Table handleClick={handleClick} />
       </Grid>
       <Grid item xs={4}>
-        <History jogadas={jogadas} />
+        <History />
       </Grid>
     </Grid>
   );
